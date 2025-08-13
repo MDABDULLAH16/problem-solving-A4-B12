25736db452f96fcfd5c242bbfdf41fe020397918
MD ABDULLAH <mdabdullah161036@gmail.com>
2025-08-13 21:47:49 +0600

    problem-1 solved

---
+++ problem-1.js
@@ -0,0 +1,11 @@
+function totalFine(fare) {
+  if ((typeof fare === "number") === true && fare > 0) {
+    const fine = fare + fare * (20 / 100) + 30;
+    return fine;
+  } else {
+    return "Invalid";
+  }
+}
+
+const result = totalFine(200);
+console.log(result);
